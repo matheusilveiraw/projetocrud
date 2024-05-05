9a810f67781c1d9a6d5ec901d410df59580a2eb6
Matheus Silveira <matheus.silveiraw@gmail.com>
2024-05-05 18:21:52 -0300

    atualizar clientes atualizado + bugs

--- editar_cliente.php
+++ editar_cliente.php
@@ -2,59 +2,52 @@
 include_once 'includes/header.php';
 include_once 'banco_de_dados/conexao_banco.php';
 
-if(isset($_GET['id'])) {
+if (isset($_GET['id'])) {
     $id = mysqli_escape_string($connect, $_GET['id']);
 
     $sql = "SELECT * FROM clientes where id = '$id'";
     $resultado = mysqli_query($connect, $sql);
     $dados = mysqli_fetch_array($resultado);
- }
+}
 ?>
-
-
-
 <div class="container text-center flex-grow-1">
-    <div class="row">
-        <div class="col"> </div>
+    <div class="row justify-content-center">
         <div class="col-10">
-            <h1>Editar de Cliente</h1>
-
+            <h1 class="text-center mt-5">Atualizar Clientes</h1>
             <form class="form-horizontal" action="banco_de_dados/update_cliente.php" method="POST">
-                <div class="col-sm-10 mt-1">
+                <div class="mt-1">
                     <input type="text" name="nome" class="form-control" id="nome" placeholder="Nome" value="<?php echo $dados['nome'] ?>">
                 </div>
-                <div class="col-sm-10 mt-1">
+                <div class="mt-1">
                     <input type="text" name="sobrenome" class="form-control" id="sobrenome" placeholder="Sobrenome" value="<?php echo $dados['sobrenome'] ?>">
                 </div>
 
-                <div class="col-sm-10 mt-1">
+                <div class="mt-1">
                     <input type="text" name="idade" class="form-control" id="Idade" placeholder="Idade" value="<?php echo $dados['idade'] ?>">
                 </div>
 
-                <div class="col-sm-10 mt-1">
-                    <input type="text" name="cpf" class="form-control" id="cpf" placeholder="CPF" value="<?php echo $dados['cpf'] ?>"> 
-                </div>
-
-                
-                <div class="col-sm-10 mt-1">
-                    <input type="hidden" name="id" class="form-control" id="id" value="<?php echo $dados['id'] ?>"> 
+                <div class="mt-1">
+                    <input type="text" name="cpf" class="form-control" id="cpf" placeholder="CPF" value="<?php echo $dados['cpf'] ?>">
                 </div>
 
 
-                <div class="container text-left mt-1">
-                <button type="text" class="btn btn-danger btn-lg col-10" name="btn-atualizar-cliente">Atualizar cliente</button>
-            </div>
+                <div class="mt-1">
+                    <input type="hidden" name="id" class="form-control" id="id" value="<?php echo $dados['id'] ?>">
+                </div>
+
+                <div class="text-center mt-1">
+                    <button type="text" class="btn btn-success btn-lg col-12" name="btn-atualizar-cliente">Atualizar cliente</button>
+                </div>
             </form>
-
-            <div class="container text-left mt-1">
-                <a class="btn btn-success btn-lg col-sm-3" href="cadastro_cliente.php" role="button">Cadastrar</a>
-
-                <a class="btn btn-warning btn-lg col-sm-3" href="index.php" role="button"">Menu</a>
+            <div class="container text-center mt-1">
+                <a class="btn btn-secondary btn-lg col-sm-3" href="clientes.php" role="button">Voltar</a>
             </div>
         </div>
-        <div class="col"> </div>
     </div>
+</div>
 
-    <?php
-    include_once 'includes/footer.php';
-    ?>
+
+
+<?php
+include_once 'includes/footer.php';
+?>
--- includes/header.php
+++ includes/header.php
@@ -22,7 +22,6 @@
   </style>
 </head>
 <body class="d-flex flex-column min-vh-100">
-  <!-- Navbar -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
       <a class="navbar-brand" href="index.php">Projeto CRUD</a>
@@ -32,7 +31,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="index.php">Início</a>
+            <a class="nav-link" href="index.php">Início</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="clientes.php">Clientes</a>
