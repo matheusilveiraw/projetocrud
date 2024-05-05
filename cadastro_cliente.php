e8ead32db7dc0f2d3c9a60ce767d7f1fe686979c
Matheus Silveira <matheus.silveiraw@gmail.com>
2024-05-05 17:37:59 -0300

    required nos campos

--- cadastro_cliente.php
+++ cadastro_cliente.php
@@ -6,27 +6,26 @@ include_once 'banco_de_dados/conexao_banco.php'
 <div class="container flex-grow-1">
     <div class="row justify-content-center">
         <div class="col-10">
-            <h1 class="text-center">Cadastro de Clientes</h1>
+            <h1 class="text-center mt-5">Cadastro de Clientes</h1>
             <form class="form-horizontal col-12" action="banco_de_dados/create_cliente.php" method="POST">
-                <div class="mt-1"> <!-- Removido col-12 -->
-                    <input type="text" name="nome" class="form-control" id="nome" placeholder="Nome">
+                <div class="mt-1"> 
+                    <input type="text" name="nome" class="form-control" id="nome" placeholder="Nome" required>
                 </div>
-                <div class="mt-1"> <!-- Removido col-12 -->
-                    <input type="text" name="sobrenome" class="form-control" id="sobrenome" placeholder="Sobrenome">
+                <div class="mt-1"> 
+                    <input type="text" name="sobrenome" class="form-control" id="sobrenome" placeholder="Sobrenome" required>
                 </div>
-                <div class="mt-1"> <!-- Removido col-12 -->
-                    <input type="text" name="idade" class="form-control" id="Idade" placeholder="Idade">
+                <div class="mt-1"> 
+                    <input type="text" name="idade" class="form-control" id="Idade" placeholder="Idade" required>
                 </div>
-                <div class="mt-1"> <!-- Removido col-12 -->
-                    <input type="text" name="cpf" class="form-control" id="cpf" placeholder="CPF">
+                <div class="mt-1"> 
+                    <input type="text" name="cpf" class="form-control" id="cpf" placeholder="CPF" required>
                 </div>
                 <div class="text-center mt-1"> 
-                    <button type="text" class="btn btn-danger btn-lg col-12" name="btn-cadastrar-cliente">Cadastrar cliente</button>
+                    <button type="text" class="btn btn-success btn-lg col-12" name="btn-cadastrar-cliente" required>Cadastrar cliente</button>
                 </div>
             </form>
             <div class="container text-center mt-1">
-                <a class="btn btn-dark btn-lg col-sm-3 me-2" href="cadastro_cliente.php" role="button">Cadastrar</a>
-                <a class="btn btn-secondary btn-lg col-sm-3" href="index.php" role="button">Menu</a>
+                <a class="btn btn-secondary btn-lg col-sm-3" href="clientes.php" role="button">Voltar</a>
             </div>
         </div>
     </div>
--- cadastro_produto.php
+++ cadastro_produto.php
@@ -3,35 +3,33 @@ include_once 'includes/header.php';
 include_once 'banco_de_dados/conexao_banco.php'
 ?>
 
-<div class="container text-center flex-grow-1">
-    <div class="row">
-        <div class="col"> </div>
+<div class="container flex-grow-1">
+<div class="row justify-content-center">
         <div class="col-10">
-            <h1>Cadastro de Produtos</h1>
+        <h1 class="text-center mt-5">Cadastro de Produtos</h1>
 
             <form class="form-horizontal" action="banco_de_dados/create_produto.php" method="POST">
-                <div class="col-sm-10 mt-1">
-                    <input type="text" name="nome" class="form-control" id="nome" placeholder="Nome">
+                <div class="mt-1">
+                    <input type="text" name="nome" class="form-control" id="nome" placeholder="Nome" required>
                 </div>
-                <div class="col-sm-10 mt-1">
-                    <input type="text" name="tipo" class="form-control" id="tipo" placeholder="Tipo">
+                <div class="mt-1">
+                    <input type="text" name="tipo" class="form-control" id="tipo" placeholder="Tipo" required>
                 </div>
 
-                <div class="container text-left mt-1">
-                <button type="text" class="btn btn-danger btn-lg col-10" name="btn-cadastrar-produto">Cadastrar produto</button>
+                <div class="text-center mt-1">
+                <button type="text" class="btn btn-success btn-lg col-12" name="btn-cadastrar-produto" required>Cadastrar produto</button>
             </div>
             </form>
 
-            <div class="container text-left mt-1">
-                <a class="btn btn-success btn-lg col-sm-3" href="produtos.php" role="button">Lista</a>
-
-                <a class="btn btn-warning btn-lg col-sm-3" href="index.php" role="button"">Menu</a>
+            <div class="container text-center mt-1">
+            <div class="container text-center mt-1">
+                <a class="btn btn-secondary btn-lg col-sm-3" href="produtos.php" role="button">Voltar</a>
+            </div>
             </div>
         </div>
-        <div class="col"> </div>
     </div>
 </div>
 
-    <?php
-    include_once 'includes/footer.php';
-    ?>
+<?php
+include_once 'includes/footer.php';
+?>
