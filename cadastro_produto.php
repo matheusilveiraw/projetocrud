d875c4b963760590eb7526e8fc6e2378f03ec610
Matheus Silveira <matheus.silveiraw@gmail.com>
2024-05-05 18:33:58 -0300

    atualizar produto att

--- cadastro_produto.php
+++ cadastro_produto.php
@@ -4,10 +4,9 @@ include_once 'banco_de_dados/conexao_banco.php'
 ?>
 
 <div class="container flex-grow-1">
-<div class="row justify-content-center">
+    <div class="row justify-content-center">
         <div class="col-10">
-        <h1 class="text-center mt-5">Cadastro de Produtos</h1>
-
+            <h1 class="text-center mt-5">Cadastro de Produtos</h1>
             <form class="form-horizontal" action="banco_de_dados/create_produto.php" method="POST">
                 <div class="mt-1">
                     <input type="text" name="nome" class="form-control" id="nome" placeholder="Nome" required>
@@ -15,16 +14,14 @@ include_once 'banco_de_dados/conexao_banco.php'
                 <div class="mt-1">
                     <input type="text" name="tipo" class="form-control" id="tipo" placeholder="Tipo" required>
                 </div>
-
-                <div class="text-center mt-1">
-                <button type="text" class="btn btn-success btn-lg col-12" name="btn-cadastrar-produto" required>Cadastrar produto</button>
-            </div>
+                <div class="mt-1">
+                    <button type="text" class="btn btn-success btn-lg col-12" name="btn-cadastrar-produto" required>Cadastrar produto</button>
+                </div>
             </form>
-
             <div class="container text-center mt-1">
-            <div class="container text-center mt-1">
-                <a class="btn btn-secondary btn-lg col-sm-3" href="produtos.php" role="button">Voltar</a>
-            </div>
+                <div class="container text-center mt-1">
+                    <a class="btn btn-secondary btn-lg col-sm-3" href="produtos.php" role="button">Voltar</a>
+                </div>
             </div>
         </div>
     </div>
