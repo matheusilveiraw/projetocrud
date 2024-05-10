3508c97ca6b505addaecf68a0dcb5daa70095b1c
Matheus Silveira <matheus.silveiraw@gmail.com>
2024-05-09 21:08:08 -0300

    mensagens de erro

--- banco_de_dados/create_cliente.php
+++ banco_de_dados/create_cliente.php
@@ -8,26 +8,41 @@ if (isset($_POST['btn-cadastrar-cliente'])) {
     $idade = mysqli_escape_string($connect, $_POST['idade']);
     $cpf = mysqli_escape_string($connect, $_POST['cpf']);
 
+    if(!preg_match('/^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/u', $nome)) { 
+        $errosNome = 'Só aceita letras e acentos!';
+    }
+
+    if(!preg_match('/^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/u', $sobrenome)) { 
+        $errosSobrenome = 'Só aceita letras e acentos!';
+    }
+
     if (preg_match('/^(\d{3}\.\d{3}\.\d{3}-\d{2}|\d{11})$/', $cpf)) {
 
         if (preg_match('/^\d{11}$/', $cpf)) $cpf = substr($cpf, 0, 3) . '.' . substr($cpf, 3, 3) . '.' . substr($cpf, 6, 3) . '-' . substr($cpf, 9, 2);
 
         $sql = "INSERT INTO clientes (nome, sobrenome, cpf, idade) VALUES ('$nome', '$sobrenome', '$cpf', '$idade')";
 
-        if(mysqli_query($connect, $sql)) {
-            $_SESSION['mensagem'] = "Cadastrado com sucesso!";
-            // header('Location: ../clientes.php?sucesso');
-        } else { 
-            $_SESSION['mensagem'] = "Erro ao cadastrar!";
-            // header('Location: ../clientes.php?erro');
-        }
-    } else {
-        $url = "cadastro_cliente.php";
-        $url .= "?nome=" . urlencode($nome);
+    } else { 
+        $errosCpf = "Formatos admitidos no CPF: 00000000000 ou 000.000.000-00";
+    }
+
+    if(isset($errosNome) || isset($errosSobrenome) || isset($errosCpf)) {
+        $url = "cadastro_cliente.php?";
+        $url .= "nome=" . urlencode($nome);
         $url .= "&sobrenome=" . urlencode($sobrenome);
         $url .= "&idade=" . urlencode($idade);
         $url .= "&cpf=" . urlencode($cpf);
-        $url .= "&msgcpf=Formatos admitidos no CPF: 00000000000 ou 000.000.000-00 ";
+        if(isset($errosNome)) $url .= "&errosNome=" . urlencode($errosNome);
+        if(isset($errosSobrenome)) $url .= "&errosSobrenome=" . urlencode($errosSobrenome);
+        if(isset($errosCpf)) $url .= "&errosCpf=" . urlencode($errosCpf);
         header('Location: ../'.$url);
+    } else { 
+        if(mysqli_query($connect, $sql)) {
+            $_SESSION['mensagem'] = "Cadastrado com sucesso!";
+            header('Location: ../clientes.php?sucesso');
+        } else { 
+            $_SESSION['mensagem'] = "Erro ao cadastrar!";
+            header('Location: ../clientes.php?erro');
+        }
     }
 }
--- cadastro_cliente.php
+++ cadastro_cliente.php
@@ -1,6 +1,10 @@
 <?php
-    include_once 'includes/header.php';
-    include_once 'banco_de_dados/conexao_banco.php';
+include_once 'includes/header.php';
+include_once 'banco_de_dados/conexao_banco.php';
+
+session_start();
+
+
 ?>
 
 <div class="container flex-grow-1">
@@ -8,20 +12,34 @@
         <div class="col-10">
             <h1 class="text-center mt-5">Cadastro de Clientes</h1>
             <form class="form-horizontal col-12" action="banco_de_dados/create_cliente.php" method="POST">
-                <div class="mt-1"> 
-                    <input type="text" name="nome" class="form-control" id="nome" placeholder="Nome" value="<?php if(isset($_GET['nome'])) echo $_GET['nome'] ?>"required>
+                <div class="mt-1">
+                    <input type="text" name="nome" class="form-control" id="nome" placeholder="Nome" value="<?php if (isset($_GET['nome'])) echo $_GET['nome'] ?>" required>
+                    <?php if (isset($_GET['errosNome'])) { ?>
+                        <span class="small-warning">
+                            <?php if (isset($_GET['errosNome'])) echo '*' . $_GET['errosNome'] ?>
+                        </span>
+                    <?php } ?>
                 </div>
-                <div class="mt-1"> 
-                    <input type="text" name="sobrenome" class="form-control" id="sobrenome" placeholder="Sobrenome" value="<?php if(isset($_GET['sobrenome'])) echo $_GET['sobrenome'] ?>" required>
+                <div class="mt-1">
+                    <input type="text" name="sobrenome" class="form-control" id="sobrenome" placeholder="Sobrenome" value="<?php if (isset($_GET['sobrenome'])) echo $_GET['sobrenome'] ?>" required>
+                    <?php if (isset($_GET['errosSobrenome'])) { ?>
+                        <span class="small-warning">
+                            <?php if (isset($_GET['errosSobrenome'])) echo '*' . $_GET['errosSobrenome'] ?>
+                        </span>
+                    <?php } ?>
                 </div>
-                <div class="mt-1"> 
-                    <input type="number" min="0" max="200" name="idade" class="form-control" id="Idade" placeholder="Idade" value="<?php if(isset($_GET['idade'])) echo $_GET['idade'] ?>" required>
+                <div class="mt-1">
+                    <input type="number" min="0" max="200" name="idade" class="form-control" id="Idade" placeholder="Idade" value="<?php if (isset($_GET['idade'])) echo $_GET['idade'] ?>" required>
                 </div>
-                <div class="mt-1"> 
-                    <input type="text" name="cpf" class="form-control" id="cpf" placeholder="CPF" value="<?php if(isset($_GET['cpf'])) echo $_GET['cpf'] ?>" required>
-                    <span class="small-warning">*<?php echo $_GET['msgcpf'] ?></span>
+                <div class="mt-1">
+                    <input type="text" name="cpf" class="form-control" id="cpf" placeholder="CPF" value="<?php if (isset($_GET['cpf'])) echo $_GET['cpf'] ?>" required>
+                    <?php if (isset($_GET['errosCpf'])) { ?>
+                        <span class="small-warning">
+                            <?php if (isset($_GET['errosCpf'])) echo '*' . $_GET['errosCpf'] ?>
+                        </span>
+                    <?php } ?>
                 </div>
-                <div class="text-center mt-1 mb-0"> 
+                <div class="text-center mt-1 mb-0">
                     <button type="text" class="btn btn-success btn-lg col-12" name="btn-cadastrar-cliente" required>Cadastrar cliente</button>
                 </div>
             </form>
@@ -33,5 +51,5 @@
 </div>
 
 <?php
-    include_once 'includes/footer.php';
+include_once 'includes/footer.php';
 ?>
